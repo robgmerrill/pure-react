f2e70ac98643fb2fb6e9c24fde3955c6bc5a4b0c
robgmerrill <robgmerrill@gmail.com>
2017-06-01 06:52:50 -0700

    added first props

--- props-tweet/src/index.js
+++ props-tweet/src/index.js
@@ -20,7 +20,7 @@ class Tweet extends React.Component {
         <Avatar hash={this.props.tweet.gravatar}/>
         <div className="content">
           <NameWithHandle/><Time/>
-          <Message/>
+          <Message text={this.props.tweet.message}/>
           <div className="buttons">
             <ReplyButton/>
             <RetweetButton/>
@@ -59,6 +59,8 @@ class Avatar extends React.Component {
       marginRight: 10
     }
     var hash = this.props.hash;
+    // the hash passed from parent to child will be attached to the end of this URL
+    // then be passed to the return statement of the component
     var url = `https://www.gravatar.com/avatar/${hash}`;
     return (
       <img src={url}
@@ -68,13 +70,22 @@ class Avatar extends React.Component {
   }
 }
 
-function Message() {
-  return(
-    <div className="message">
-      This is less than 140 characters.
-    </div>
-  );
-}
+// function Message() {
+//   return(
+//     <div className="message">
+//       {this.props.text}
+//     </div>
+//   );
+// }
+
+// WHY?? DO STATELESS FUNCTIONAL COMPONENTS NOT WORK HERE?
+var Message = React.createClass({
+  render: function() {
+    return (
+      <div className="message">
+        {this.props.text}
+        </div> );
+} });
 
 class NameWithHandle extends React.Component {
   render() {
@@ -126,5 +137,7 @@ class MoreOptionsButton extends React.Component {
 }
 
 
-
+// the attribut of "tweet" is passed to our component. It is an object. So now we can access
+// all the property values of that object with tweet.message or tweet.avatar and so on.
+// pain point. Learning that Stateless functional components don't handle props...
 ReactDOM.render(<Tweet tweet={testTweet}/>, document.getElementById('root'));
